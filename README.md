55bbb6b0290372bec44c2f18436ed88eddfe5dd7
危文才 <weiwencai@qudian.com>
2021-02-19 18:58:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebibibi
+firebibibi
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+huobi_package_path = os.path.join(current_dir, 'huobi_Python')
+sys.path.append(huobi_package_path)
+
+
+if __name__ == '__main__':
+
+
+    pass
+
+
+
+
+
+
+
+
+
