b52e938a24fe81d0bd399ec86171655a9d6b6d76
危文才 <weiwencai@qudian.com>
2021-02-26 01:34:55 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # FIREBIBIBI
 ## deploy
 ```bash
-cd src && git clone git@github.com:HuobiRDCenter/huobi_Python.git
+pip3 install -r requirement.text
 ```
 
 ## run
 ```bash
-cd src 
-python3 main.py
-# or
-./run.sh
+cd src; python3 main.py
 ```
 
 ## firebibibi
 firebibibi
+
+## reference
+- [huobi API DOCUMENT](https://huobiapi.github.io/docs/spot/v1/cn/#5ea2e0cde2)
+- [huobi_python github](https://github.com/HuobiRDCenter/huobi_Python)
--- src/main.py
+++ src/main.py
@@ -1,39 +1,11 @@
-import sys
-import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-huobi_package_path = os.path.join(current_dir, 'huobi_Python')
-sys.path.append(huobi_package_path)  # 火币的库加入包的搜索路径
-
+import time
+from datetime import datetime
 from huobi.client.market import MarketClient
 from huobi.constant import CandlestickInterval
-from huobi.utils import LogInfo
 
-from huobi.exception.huobi_api_exception import HuobiApiException
-from huobi.model.market.candlestick_event import CandlestickEvent
 
-class Strategy:
-    DIRECTION_UP = 1
-    DIRECTION_DOWN = 0
-    DIRECTION_UNKNOWN = -1
-
-    def __init__(self, symbol='ethusdt', interval=CandlestickInterval.MIN1, trigger_num=3) -> None:
-        self.trigger_num = trigger_num  # 触发的连续次数
-
-        self.direction = -1  # 0：下跌   1：上涨  -1：未知
-        self.pre_direction = -1
-        self.continuous_times = 0  # 连续数量
-        self.cuttent_time = 0
-        self.next_time = 0
-        
-        self.symbol = symbol
-        self.interval = interval
-        self.client = self._get_client()
-        self.miss_cnt = 0
-        self.pre_quote = None
-        self.interval_seconds = self._get_interval_seconds()
-
-    def _get_interval_seconds(self):
-        if self.interval ==CandlestickInterval.MIN1:
+def _get_interval_seconds(self):
+        if self.interval == CandlestickInterval.MIN1:
             return 60 
         elif self.interval == CandlestickInterval.MIN5:
             return 60 * 5
@@ -50,92 +22,178 @@ class Strategy:
         elif self.interval == CandlestickInterval.YEAR1:
             return 60 * 60 * 24 * 365
         return 60
+class Direction:
+    DIRECTION_UP = 1
+    DIRECTION_DOWN = 0
+    DIRECTION_UNKNOWN = -1
+
+    @classmethod
+    def get_direction_label(cls, direction):
+        if cls.DIRECTION_UP == direction:
+            return '上涨'
+        elif cls.DIRECTION_DOWN == direction:
+            return '下跌'
+        else:
+            return '未知'
+
+
+class DirectionStaus:
+    def __init__(self, direction=-1, continuous_times=0, low=0, high=0) -> None:
+        self.direction = direction
+        self.continuous_times = continuous_times  # 相同方向连续次数
+        self.low = low
+        self.high = high
+        self.timestamp = 0
+
+    @property
+    def direction_label(self):
+        return Direction.get_direction_label(self.direction)
+
+    def echo(self, prefix=''):
+        print(
+            f'{prefix}方向：{self.direction_label}，' +
+            f'{prefix}次数：{self.continuous_times}，' +
+            f'{prefix}最高：{self.high}，' +
+            f'{prefix}最低：{self.low}，' +
+            f'{prefix}时间：{datetime.fromtimestamp(self.timestamp)}，' +
+            '' 
+        )
+
+class StrategyDimension:
+    def __init__(self, direction=Direction.DIRECTION_DOWN, trigger_times=4, reverse_trigger_times=1, remind_times=2) -> None:
+        self.direction = direction
+        self.trigger_times = trigger_times  # 连续次数后才能触发
+        self.reverse_trigger_times = reverse_trigger_times  # 反向的出现几次后触发
+        self.remain_times = remind_times  # 提醒下单的次数（一个tick提醒一次）
+    
+    @property
+    def direction_label(self):
+        return Direction.get_direction_label(self.direction)
+    
+class Strategy:
+    def __init__(self, symbol='ethusdt', interval=CandlestickInterval.MIN1, dimension=None, min_get_interval=5) -> None:
+        self.pre_direction_status = DirectionStaus()
+        self.curr_direction_status = DirectionStaus()
+        self.dimension = dimension or StrategyDimension()
+
+        self.direction_changed = False
+        self.has_new = False
+        
+        self.symbol = symbol
+        self.interval = interval
+        self.client = self._get_client()
+        self.interval_seconds = _get_interval_seconds(self=self)
+        self.min_get_interval = min_get_interval
+        self.need_print = True
         
     def _get_client(self):
         market_client = MarketClient(init_log=True)
         return market_client
 
-    def get_quote(self):
-        list_obj = self.client.get_candlestick(self.symbol, self.interval, 2)
-        
-        LogInfo.output(f"---- {self.interval} candlestick for {self.symbol} ----")
-        LogInfo.output_list(list_obj)
+    def get_quote(self, size):
+        list_obj = self.client.get_candlestick(self.symbol, self.interval, size)
         return list_obj
-    
-    def _handle_first_quote(self, quote):
-        self.cuttent_time = quote.Id
-        self.next_time = quote.id + self.interval_seconds
-        self.pre_quote = quote
-        if quote.Close > quote.Open:
-            self.direction = self.DIRECTION_UP
-        else:
-            self.direction = self.DIRECTION_DOWN
-
-        self.continuous_times = 1  # 连续数量
-        self.cuttent_time = quote.Id
-        self.next_time = quote.Id + self.interval_seconds
-        self.pre_direction = self.direction
-
 
     def _handle_quote(self, quote):
-        self.cuttent_time = quote.Id
-            
-        # 上涨：收盘 > 开盘
-        # 下跌：收盘 < 开盘
-        if quote.Close > quote.Open:
-            self.direction = self.DIRECTION_UP
+        if quote.id <= self.curr_direction_status.timestamp:
+            old_time = datetime.fromtimestamp(self.curr_direction_status.timestamp)
+            new_time = datetime.fromtimestamp(quote.id)
+            print(f'没有新行情，行情时间{new_time}，原时间：{old_time}')
+            self.has_new = False
+            return
+        self.has_new = True
+        # 上涨：收盘 >= 开盘  下跌：收盘 < 开盘
+        if quote.close >= quote.open:
+            direction = Direction.DIRECTION_UP
         else:
-            self.direction = self.DIRECTION_DOWN
-        
+            direction = Direction.DIRECTION_DOWN
 
-        if self.direction == self.pre_direction:
-            self.continuous_times += 1
+        if direction == self.curr_direction_status.direction:  # 同向
+            self.direction_changed = False
+            self.curr_direction_status.continuous_times += 1
+
+            self.curr_direction_status.low = min(quote.low, self.curr_direction_status.low)
+            self.curr_direction_status.high = max(quote.low, self.curr_direction_status.high)
+
         else:
-            self.continuous_times = 1
+            self.direction_changed = True
+            direction_status = DirectionStaus()
+            direction_status.continuous_times += 1
+            direction_status.direction = direction
+            direction_status.low = quote.low
+            direction_status.high = quote.high
 
-        self.pre_direction = self.direction
-        self.pre_quote = quote
-        self.next_time = quote.id + self.interval_seconds
+            self.pre_direction_status = self.curr_direction_status
+            self.curr_direction_status = direction_status
+
+        self.curr_direction_status.timestamp = quote.id
+        self.print_status()
+
+    def print_status(self):
+        self.pre_direction_status.echo(prefix='前')
+        self.curr_direction_status.echo(prefix='现')
+
+    def handle_trade_strategy(self):
+        if self.pre_direction_status.direction != self.dimension.direction:
+            self._print(f'行情方向不符，实际：{self.pre_direction_status.direction_label}，标准：{self.dimension.direction_label}')
+            return
+
+        if self.pre_direction_status.continuous_times < self.dimension.trigger_times:
+            self._print(f'连续次数不够，实际：{self.pre_direction_status.continuous_times}，标准：{self.dimension.trigger_times}')
+            return
         
+        if self.curr_direction_status.continuous_times < self.dimension.reverse_trigger_times:
+            self._print(f'反向连续次数不够，实际：{self.curr_direction_status.continuous_times}，标准：{self.dimension.reverse_trigger_times}')
+            return
 
-        print(f'方向：{self.direction}，次数：{self.continuous_times}')
+        if self.curr_direction_status.continuous_times < self.dimension.reverse_trigger_times + self.dimension.remain_times:
+            self._order()
+    
+    def _order(self):
+        print('卧槽好时机，下单了****************')
 
+    def _print(self, content):
+        if self.need_print:
+            print(content)
 
-    def _get_direction_str(self):
-        if self.direction == self.DIRECTION_UP:
-            return '上涨'
-        elif self.direction == self.DIRECTION_DOWN:
-            return '下跌'
+    def _get_sleep_seconds(self):
+        curr_quote_timestamp = self.curr_direction_status.timestamp
+        next_quote_timeStamp = curr_quote_timestamp + self.interval_seconds
+        curr_timestamp = int(time.time())
+        print('next time: ', datetime.fromtimestamp(next_quote_timeStamp))
+        print('curr time: ', datetime.fromtimestamp(curr_timestamp))
+        delta_seconds = next_quote_timeStamp - curr_timestamp
+        print('delta seconds: ', delta_seconds)
+        if delta_seconds > self.interval_seconds:  # sleep时间小于一个tick时间，说明少了一根线，马上获取行情
+            return 0
         else:
-            return '未知方向'
-        
-        
-
-        
-
-
-
+            return delta_seconds
+       
     def run(self):
-        # 获取n分钟的行情
-        list_obj = self.get_quote()
-        for obj in list_obj:
+        while True:
+            try:
+                list_obj = self.get_quote(size=1)
+            except Exception as e:
+                print(e)
+                print(f'获取行情失败，{self.min_get_interval} 秒后再次获取。\n')
+                time.sleep(self.min_get_interval)
+                continue
+
+            obj = list_obj[0]
             self._handle_quote(obj)
-
-
-
+            if self.has_new:
+                self.handle_trade_strategy()
+            seconds = self._get_sleep_seconds()
+            if seconds < self.min_get_interval:
+                print(f'限速了，{self.min_get_interval} 秒后获取行情。\n')
+                time.sleep(self.min_get_interval)
+            else:
+                print(f'sleep: {seconds}')
+                time.sleep(seconds)
+            print()
 
 
 if __name__ == '__main__':
-    n = 3
-    s = Strategy()
+    dimension = StrategyDimension(direction=Direction.DIRECTION_DOWN, trigger_times=4, reverse_trigger_times=1, remind_times=2)
+    s = Strategy(symbol='ethusdt', interval=CandlestickInterval.MIN1, dimension=dimension, min_get_interval=2)
     s.run()
-
-
-
-
-
-
-
-
-
-
